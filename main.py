eb59b23d3a90d3ab027a2e53461fbec0ab1909be
George Rolland <wellnessbykingrollandscorner@gmail.com>
2025-05-18 18:12:30 -0400

    Initial working bot setup

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+
+def run_bot():
+    print("Rolland Quantum Trader is now active.")
+    while True:
+        print("Fetching new market data...")
+        time.sleep(10)
+
+if __name__ == "__main__":
+    run_bot()
